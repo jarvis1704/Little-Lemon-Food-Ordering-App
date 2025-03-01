4f2731bb6fb6fea139c4d1e559034ed34a2d865b
Biprangshu Das <dasbiprangshu@gmail.com>
2025-03-01 18:43:05 +0530

    Added Dynamic header function

--- app/src/main/java/com/biprangshu/littlelemonapp/homescreen/HomeScreen.kt
+++ app/src/main/java/com/biprangshu/littlelemonapp/homescreen/HomeScreen.kt
@@ -1,9 +1,103 @@
 package com.biprangshu.littlelemonapp.homescreen
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.biprangshu.littlelemonapp.R
 
 @Composable
 fun homeScreen(modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+                .navigationBarsPadding()
+        ) {
 
+        }
+    }
 }
+
+
+@Composable
+fun dynamicHeader(navController: NavController, scrolledUp: Boolean, onSearch: (String)-> Unit) {
+    val headerHeightExpanded=150.dp
+    val headerHeightCollapsed=56.dp
+
+    val headerHeight by animateDpAsState(
+        targetValue = if(scrolledUp) headerHeightExpanded else headerHeightCollapsed,
+        animationSpec = tween(500)
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(headerHeight)
+            .background(MaterialTheme.colorScheme.background) // Ensure header background is consistent
+    ) {
+        AnimatedVisibility(
+            visible = scrolledUp,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            ExpandedHeaderContent(navController = navController, onSearch = onSearch)
+        }
+        AnimatedVisibility(
+            visible = !scrolledUp,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            CollapsedHeaderContent()
+        }
+    }
+}
+
+@Composable
+fun CollapsedHeaderContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(painter = painterResource(R.drawable.little_lemon_logo,), contentDescription = "Little Lemon Logo", contentScale = ContentScale.Crop)
+    }
+}
+
+@Composable
+fun ExpandedHeaderContent(navController: NavController, onSearch: (String) -> Unit) {
+    TODO("Not yet implemented")
+}
+
+//@Preview(showBackground = true, showSystemUi = true)
+//@Composable
+//private fun homeScreenPrev() {
+//    CollapsedHeaderContent()
+//}
