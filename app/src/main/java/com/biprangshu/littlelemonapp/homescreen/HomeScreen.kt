db489c99819b976af2154d1025ebea1929f3f89b
Biprangshu Das <dasbiprangshu@gmail.com>
2025-03-01 19:37:03 +0530

    Added the layout for futher elements

--- app/src/main/java/com/biprangshu/littlelemonapp/homescreen/HomeScreen.kt
+++ app/src/main/java/com/biprangshu/littlelemonapp/homescreen/HomeScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -93,7 +95,14 @@ fun CollapsedHeaderContent() {
 
 @Composable
 fun ExpandedHeaderContent(navController: NavController, onSearch: (String) -> Unit) {
-    TODO("Not yet implemented")
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ) {
+        Row {
+            Text("Hello User!")
+
+        }
+    }
 }
 
 //@Preview(showBackground = true, showSystemUi = true)
