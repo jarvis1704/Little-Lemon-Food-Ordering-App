22148fbf272024a6e2435715eef5018c16c95ef3
Biprangshu Das <dasbiprangshu@gmail.com>
2025-03-06 12:07:11 +0530

    Changed the Manifest to add Application Class

---
+++ app/src/main/java/com/biprangshu/littlelemonapp/LittleLemonApplication.kt
@@ -0,0 +1,8 @@
+package com.biprangshu.littlelemonapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class LittleLemonApplication: Application() {
+}
